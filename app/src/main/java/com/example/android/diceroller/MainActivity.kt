1d9dab077016e879c75797f26f2a9501dc639135
Toshihiko Doi <36196747+ishot-l@users.noreply.github.com>
2020-02-29 21:17:37 +0900

    Fundamentals 01.3.5

--- app/src/main/java/com/example/android/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/android/diceroller/MainActivity.kt
@@ -3,40 +3,41 @@ package com.example.android.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var diceImage : ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        diceImage = findViewById(R.id.dice_image)
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
 
-        val countUpButton: Button = findViewById(R.id.countup_button)
-        countUpButton.setOnClickListener { countUp() }
     }
 
     private fun rollDice() {
-        // Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
 
-        val resultText: TextView = findViewById(R.id.result_text)
         val randomInt = (1..6).random()
 
-        resultText.text = randomInt.toString()
-
-    }
-
-    private fun countUp() {
-        val resultText: TextView = findViewById(R.id.result_text)
-
-        resultText.text = when(resultText.text) {
-            getString(R.string.hello_world) -> "1"
-            "1", "2", "3", "4", "5" -> (resultText.text.toString().toInt() + 1).toString()
-             else -> resultText.text
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
+
+        diceImage.setImageResource(drawableResource)
     }
+
 }
